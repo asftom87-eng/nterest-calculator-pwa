524d9b06eeeecec035fdc0d700813c7c4a0c65ab
asftom87-eng <asftom87@gmail.com>
2025-12-18 21:11:16 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,52 @@
-// 每次修改程式碼，請把 v1 改成 v2, v3... 以此類推
-const CACHE_NAME = 'interest-calc-v2'; 
+const CACHE_NAME = 'interest-calculator-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './script.js',
-    './manifest.json'
+    '/', // 根目錄 (通常是 index.html)
+    '/index.html',
+    '/script.js',
+    '/manifest.json',
+    '/images/icon-192x192.png', // 確保圖標也被緩存
+    '/images/icon-512x512.png',
+    // 如果您有 CSS 檔案 (例如 style.css)，請加上它
+    // '/style.css' 
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+// 安裝 Service Worker 並快取所有資源
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+// 攔截網路請求，嘗試從快取中獲取資源
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keys) => Promise.all(
-        keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); })
-    )));
+// 清理舊的快取
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
