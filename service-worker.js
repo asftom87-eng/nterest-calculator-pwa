7746fea8e221efa911244d50a3d33d0777feb3fd
asftom87-eng <asftom87@gmail.com>
2025-12-18 21:18:25 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'interest-v20';
+const CACHE_NAME = 'interest-v30';
 const urls = ['./', './index.html', './script.js', './manifest.json'];
 self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(urls))));
 self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
